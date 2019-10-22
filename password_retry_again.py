80952cf7e780f110c934249e4cf59cf50c0bb183
李明叡 <leeray1208@limingruide-MacBook-Pro.local>
2019-10-21 22:10:45 -0700

    change to while chances > 0

--- password_retry_again.py
+++ password_retry_again.py
@@ -5,9 +5,10 @@
 # if not correct, print 'wrong password! have __ chances'
 # correct, print 'log in successfully'
 
+# 進階挑戰 不要寫while True, 要寫條件
 password = 'a123456'
 chances = 3	# remain chances
-while True:
+while chances > 0:
 	code = input('Please enter password: ')  # already use password, do not overwrite
 	if code == password:
 		print('Log in successfully!')
@@ -15,7 +16,8 @@ while True:
 	else:
 		chances = chances - 1	# -1之後存回chances
 		print('Wrong password! You have %d chances' %chances)
-		if chances == 0:
-			break	# 沒剩餘機會 跳出
+		#if chances == 0:
+		#	break	# 沒剩餘機會 跳出
+		# 少了兩行 程式更簡潔
 		
 
